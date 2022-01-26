05c8ea676ccb13b907c141a17e7d601df7d76eef
rahulsingh2711 <singhrahulkumar564@gmail.com>
2022-01-26 14:28:54 +0530

    v2

--- App.js
+++ App.js
@@ -1,49 +1,52 @@
 import React, { Component } from 'react';
 import QRCodeScanner from 'react-native-qrcode-scanner';
-import {RNCamera} from 'react-native-camera'
-import { Text, View ,TouchableOpacity,StyleSheet} from 'react-native';
+import { RNCamera } from 'react-native-camera'
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
 export default class App extends Component {
   state = {
-    qr:""
+    qr: ""
   }
 
   onRead = e => {
-    this.setState({qr:e.data})
-   
+    this.setState({ qr: e.data })
+
   }
-  
-  
+
+
   render() {
     return (
       <View style={styles.container}>
-      <QRCodeScanner
+        <QRCodeScanner
           onRead={this.onRead}
           // flashMode={RNCamera.Constants.FlashMode.torch}
           topContent={
             <Text>
               {this.state.qr
               }
-              
+
             </Text>
           }
           bottomContent={
-            <TouchableOpacity>
-              <Text>OK. Got it!</Text>
-            </TouchableOpacity>
+            <View>
+              {this.state.qr ? <QRCode
+                value={this.state.qr}
+              /> : null}
+            </View>
           }
         />
-        </View>
-     
+      </View>
+
     );
   }
 }
 
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:'grey',
-    
+  container: {
+    flex: 1,
+    backgroundColor: 'grey',
+
   }
 })
