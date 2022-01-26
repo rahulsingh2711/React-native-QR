ae9273524630c3671dcdc96660901dace5f5596e
rahulsingh2711 <singhrahulkumar564@gmail.com>
2022-01-26 14:12:36 +0530

    update

--- App.js
+++ App.js
@@ -4,17 +4,27 @@ import {RNCamera} from 'react-native-camera'
 import { Text, View ,TouchableOpacity,StyleSheet} from 'react-native';
 
 export default class App extends Component {
+  state = {
+    qr:""
+  }
+
+  onRead = e => {
+    this.setState({qr:e.data})
+   
+  }
+  
+  
   render() {
     return (
       <View style={styles.container}>
       <QRCodeScanner
-          onRead={this.onSuccess}
-          flashMode={RNCamera.Constants.FlashMode.torch}
+          onRead={this.onRead}
+          // flashMode={RNCamera.Constants.FlashMode.torch}
           topContent={
             <Text>
-              Go to{' '}
-              <Text>wikipedia.org/wiki/QR_code</Text> on
-              your computer and scan the QR code.
+              {this.state.qr
+              }
+              
             </Text>
           }
           bottomContent={
@@ -33,7 +43,7 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    backgroundColor:'black',
+    backgroundColor:'grey',
     
   }
 })
